669eacbfc2243abf4d75863db3f3b3d46a543464
aligh <alighanbari113@gmail.com>
2023-04-21 13:57:50 +0330

    added laravel/ui-bootstrap

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,15 +6,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>انجمن X | @yield('title')</title>
+    @vite(['resources/js/app.js'])
+    <script src="{{asset('js/main.js')}}"></script>
+    @vite(['resources/sass/app.scss'])
+    <link rel="stylesheet" href="{{asset("css/main.css")}}">
 </head>
 
 <body>
     @includeIf('../partials.header')
+    <button class="btn btn-secondary m-4 p-3">submit</button>
 
 
     @yield('content')
 
-    
+
     @includeIf('../partials.footer')
 
 
