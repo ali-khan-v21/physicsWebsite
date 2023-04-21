026a5e4b3da2d35ea0da223ee12f529fc5b6fda3
aligh <alighanbari113@gmail.com>
2023-04-21 09:49:03 +0330

    master blade-503 construction page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.master')
+@section('title','home page')
+@section('content')
+<h1>index page</h1>
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae
+    , ex consequatur et cumque at error consectetur, nemo nisi reici
+    endis temporibus veniam tempora molestiae corporis iusto. Quo, labore e
+    st, voluptatibus eos facere distinctio fuga necessitatibus, quos quam iste ex
+    ercitationem ducimus. Impedit quibusdam suscipit magni, eligendi veritatis ab
+     laborum eos veniam id.</p>
+@endsection
