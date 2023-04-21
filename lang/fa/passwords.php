ad71940599a931b98d3ffabe4f43488cf57aacc0
aligh <alighanbari113@gmail.com>
2023-04-21 10:47:10 +0330

    translated lang/fa

--- lang/fa/passwords.php
+++ lang/fa/passwords.php
@@ -2,21 +2,11 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Password Reset Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are the default lines which match reasons
-    | that are given by the password broker for a password update attempt
-    | has failed, such as for an invalid token or invalid new password.
-    |
-    */
 
-    'reset' => 'Your password has been reset.',
-    'sent' => 'We have emailed your password reset link.',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'reset' => 'پسورد شما با موفقیت تغییر کرد',
+    'sent' => 'لینک بازنشانی پسورد شما ایمیل شد',
+    'throttled' => 'لطفا پس از تلاش دوباره اندکی صبر کنید.',
+    'token' => 'توکن بازنشانی پسورد صحیح نمی باشد.',
+    'user' => "کاربری با این ایمیل یافت نشد.",
 
 ];
