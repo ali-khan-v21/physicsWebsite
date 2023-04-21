f5afd8c81a8920f90154dcc2b5212396baaa24cb
aligh <alighanbari113@gmail.com>
2023-04-21 09:59:14 +0330

    master blade -503 under construction

---
+++ resources/views/contactus.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.master')
+@section('title',"contact us page")
+@section('content')
+<h1>contact us page</h1>
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae
+    , ex consequatur et cumque at error consectetur, nemo nisi reici
+    endis temporibus veniam tempora molestiae corporis iusto. Quo, labore e
+    st, voluptatibus eos facere distinctio fuga necessitatibus, quos quam iste ex
+    ercitationem ducimus. Impedit quibusdam suscipit magni, eligendi veritatis ab
+     laborum eos veniam id.</p>
+@endsection
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>انجمن X | @yield('title')</title>
+</head>
+
+<body>
+    <header>
+        <h2>header here</h2>
+    </header>
+
+    @yield('content')
+
+
+    <footer>
+        <h3>footer here</h3>
+    </footer>
+
+</body>
+
+</html>
