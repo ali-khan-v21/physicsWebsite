80776553290b3a53e192afd421853aa3ac50aa2e
aligh <alighanbari113@gmail.com>
2023-04-21 12:54:25 +0330

    added header and footer to partials

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,29 +9,16 @@
 </head>
 
 <body>
-    <header>
+    @includeIf('../partials.header')
 
-
-        <ul>
-            <li><a href="/" target="_self"> {{ __('public.home') }} </a></li>
-            <li><a href="/about" target="_self"> {{ __('public.about') }} </a></li>
-            <li><a href="/contactus" target="_self"> {{ __('public.contact') }} </a></li>
-            <ul>
-
-                <li><a href="#" target="_self"> {{ __('public.language') }} </a></li>
-                @foreach (config('app.available_lacales') as $locale)
-                    <li><a href=/lang/{{$locale}}> {{ __('public.'.$locale) }} </a></li>
-                @endforeach
-            </ul>
-        </ul>
-    </header>
 
     @yield('content')
 
+    
+    @includeIf('../partials.footer')
 
-    <footer>
-        <h3>footer here</h3>
-    </footer>
+
+
 
 </body>
 
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,7 @@
+<footer>
+    <h3>footer here</h3>
+    <ul>
+        <li>one</li>
+        <li>two</li>
+    </ul>
+</footer>
